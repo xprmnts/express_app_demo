3d0460b6ec5c20e25eae7d8082374f6fd43d7157
xprmnts <abhinay@outlook.com>
2021-03-06 10:06:17 -0500

    mvc and writing / reading from files

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    // console.log('In another middleware!');
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
